e185f4d22b3121bf79e707f2551eed580bb738e5
wjdgusdjssld-art <wjdgusdjssld@gmail.com>
2026-06-19 19:26:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/proxy', methods=['POST'])
+def add_cors(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, X-Naver-Client-Id, X-Naver-Client-Secret'
+    return response
+
+@app.after_request
+def after_request(response):
+    return add_cors(response)
+
+@app.route('/proxy', methods=['GET', 'POST', 'OPTIONS'])
 def proxy():
+    if request.method == 'OPTIONS':
+        return jsonify({}), 200
+    
     client_id = request.headers.get('X-Naver-Client-Id')
     client_secret = request.headers.get('X-Naver-Client-Secret')
     
@@ -17,18 +30,7 @@ def proxy():
         },
         json=request.get_json()
     )
-    
-    response = jsonify(res.json())
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response, res.status_code
-
-@app.route('/proxy', methods=['OPTIONS'])
-def proxy_options():
-    response = jsonify({})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
-    return response, 200
+    return jsonify(res.json()), res.status_code
 
 if __name__ == '__main__':
     app.run()
